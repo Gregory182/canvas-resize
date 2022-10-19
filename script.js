b83d4fd54088123038bdb167d6d4f1474379c5db
Gregory182 <rozpedzik@gmail.com>
2022-10-19 17:49:15 +0200

    add download function

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const button = document.getElementById('draw')
+const downloadBtn = document.getElementById('download')
 const canvas = document.getElementById('canvas')
 const opacityVal = document.getElementById('opacityValue')
 const imgSizeInput = document.getElementById('img-size')
 
 
+let maxSize = imgSizeInput.value
+let opacity = 1
 
-const drawImageToCanvas = (maxSize, alpha) => {
+const drawImageToCanvas = async (maxSize, alpha) => {
     const ctx = canvas.getContext('2d')
     const image = new Image()
     const wm = new Image()
     image.src = 'img/photo2.jpg'
     wm.src = 'img/wm2.png'
-    
+
     let width = image.width
     let height = image.height
 
@@ -31,8 +34,8 @@ const drawImageToCanvas = (maxSize, alpha) => {
 
     console.log(width, height)
     ctx.drawImage(image, 0, 0, width, height)
-    
-    const ptrn = ctx.createPattern(wm,'repeat')
+
+    const ptrn = ctx.createPattern(wm, 'repeat')
 
     ctx.fillStyle = ptrn;
     ctx.globalAlpha = alpha
@@ -41,21 +44,41 @@ const drawImageToCanvas = (maxSize, alpha) => {
 }
 
 
-let minVal = imgSizeInput.value
-let opacity = 1
-const reDraw = ()=>{
-
-    drawImageToCanvas(minVal,opacity)
+const reDraw = () => {
+    drawImageToCanvas(maxSize, opacity)
 }
 
-button.addEventListener('click', ()=> reDraw())
-opacityVal.addEventListener('input',(e)=> {
-    opacity=e.target.value;
+const saveAsFile = () => {
+    const link = document.createElement('a');
+    link.download = 'download.png';
+    link.href = canvas.toDataURL();
+    link.click();
+    link.delete;
+
+}
+
+function DownloadCanvasAsImage(){
+    let downloadLink = document.createElement('a');
+    downloadLink.setAttribute('download', 'CanvasAsImage.png');
+    let canvas = document.getElementById('canvas');
+    canvas.toBlob(function(blob) {
+      let url = URL.createObjectURL(blob);
+      downloadLink.setAttribute('href', url);
+      downloadLink.click();
+    });
+}
+
+button.addEventListener('click', () => reDraw())
+
+opacityVal.addEventListener('input', (e) => {
+    opacity = e.target.value;
     reDraw()
 })
 
-imgSizeInput.addEventListener('change', (e)=>{
-    minVal=e.target.value
+imgSizeInput.addEventListener('change', (e) => {
+    maxSize = e.target.value
     reDraw()
 })
-drawImageToCanvas(minVal,opacity)
+
+downloadBtn.addEventListener('click',DownloadCanvasAsImage)
+drawImageToCanvas(maxSize, opacity)
